8e02ce4bd348e6ef9c7a0791188ac011a9bb0742
Alexisay97 <43943071+Alexisay97@users.noreply.github.com>
2021-10-25 16:20:49 -0600

    Update users migrate
    Se creo un usuario por defecto con el tipo administrador para cuando sea necesario hacer un migrate se pueda ingresar sin problema al sistema

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Schema;
 
 class CreateUsersTable extends Migration
@@ -24,6 +27,17 @@ class CreateUsersTable extends Migration
             $table->rememberToken();
             $table->timestamps();
         });
+
+        $user = new User();
+
+        $user->name = 'Usuario Default';
+        $user->email = 'admin@mail.com';
+        $user->password = Hash::make('12345678');
+        $user->role = '1';
+        $user->empleado_id = '0';
+
+        $user->save();
+        
     }
 
     /**
