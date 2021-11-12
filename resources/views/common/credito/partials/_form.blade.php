7f69db4de2e89e22a840734e83bacf0663f4f6a0
Ezio2220 <fjosue447@gmail.com>
2021-11-12 15:32:07 -0600

    creditos
    funcion rentas.js nose porque le puse asi pero asi que quede XD en realidad lo que contiene es la funcion del calculo de las cuotas de creditos por medio de js, tambien incluyo cambios en lo campos del formulario para que utilizen esta funcion y dispongan de otras validaciones.

--- resources/views/common/credito/partials/_form.blade.php
+++ resources/views/common/credito/partials/_form.blade.php
@@ -1,38 +1,38 @@
 <div class="form-group col-md-4 first">
     <label for="fecha">Fecha</label>
-    <input type="date" accept="any" name="fecha" class="form-control @error('fecha') is-invalid 
-    @enderror" id="fecha" palceholder="$" value="{{old('fecha')}}">
+    <input type="date" accept="any" name="fecha" class="form-control @error('fecha') is-invalid
+    @enderror" id="fecha" palceholder="$" onchange="calc(this);" value="{{old('fecha')}}">
     @error('fecha')
     <div class="invalid-feedback">{{ $message }}</div>
     @enderror
 </div>
 <div class="form-group col-md-4 first">
     <label for="monto">Monto</label>
-    <input type="number" accept="any" name="monto" class="form-control @error('monto') is-invalid 
-    @enderror" id="monto" palceholder="$" value="{{old('monto')}}">
+    <input type="number" min=1 accept="any" name="monto" class="form-control @error('monto') is-invalid
+    @enderror" onchange="calc(this);" id="monto" palceholder="$" value="{{old('monto')}}">
     @error('monto')
     <div class="invalid-feedback">{{ $message }}</div>
     @enderror
 </div>
 <div class="form-group col-md-4 first">
     <label for="dias">Dias Facturados</label>
-    <input type="number" accept="any" name="dias" class="form-control @error('dias') is-invalid 
-    @enderror" id="dias" palceholder="$" value="{{old('dias')}}">
+    <input type="number" min=1 max=30 accept="any" name="dias" class="form-control @error('dias') is-invalid
+    @enderror" onchange="calc(this);" id="dias" palceholder="$" value="{{old('dias')}}">
     @error('dias')
     <div class="invalid-feedback">{{ $message }}</div>
     @enderror
 </div>
 <div class="form-group col-md-4 first">
-    <label for="dias_mora">Dias de Mora</label>
-    <input type="number" accept="any" name="dias_mora" class="form-control @error('dias_mora') is-invalid 
-    @enderror" id="dias_mora" value="{{old('dias_mora')}}">
-    @error('dias_mora')
+    <label for="mora">$ Mora</label>
+    <input type="number" accept="any" min=0 name="mora" class="form-control @error('mora') is-invalid
+    @enderror" onchange="calc(this);" id="mora" value="{{old('mora')}}">
+    @error('mora')
     <div class="invalid-feedback">{{ $message }}</div>
     @enderror
 </div>
 <div class="form-group col-md-4 first">
     <label for="interes">Interes</label>
-    <input type="number" accept="any" name="interes" class="form-control @error('interes') is-invalid 
+    <input type="number" accept="any" name="interes" class="form-control @error('interes') is-invalid
     @enderror" id="interes" value="{{old('interes')}}" readonly>
     @error('interes')
     <div class="invalid-feedback">{{ $message }}</div>
@@ -40,7 +40,7 @@
 </div>
 <div class="form-group col-md-4 first">
     <label for="abonado">Total abonado</label>
-    <input type="number" accept="any" name="abonado" class="form-control @error('abonado') is-invalid 
+    <input type="number" accept="any" name="abonado" class="form-control @error('abonado') is-invalid
     @enderror" id="abonado" value="{{old('abonado')}}" readonly>
     @error('abonado')
     <div class="invalid-feedback">{{ $message }}</div>
@@ -48,7 +48,7 @@
 </div>
 <div class="form-group col-md-4 first">
     <label for="saldo">Nuevo Saldo</label>
-    <input type="number" accept="any" name="saldo" class="form-control @error('saldo') is-invalid 
+    <input type="number" accept="any" name="saldo" class="form-control @error('saldo') is-invalid
     @enderror" id="saldo" value="{{old('saldo')}}" readonly>
     @error('saldo')
     <div class="invalid-feedback">{{ $message }}</div>
