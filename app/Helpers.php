6488d502700033d7778d5cb104c2d8a98b9d2036
Alexisay97 <43943071+Alexisay97@users.noreply.github.com>
2021-11-26 16:56:20 -0600

    Arreglando bugs

--- app/Helpers.php
+++ app/Helpers.php
@@ -29,21 +29,26 @@ function delete_files($dir) {
 
 function upload_global($file, $folder,$name = null,$w = null,$h = null){ 
 
+    
     $file_type = $file->getClientOriginalExtension(); 
     $folder = $folder; 
     $destinationPath = public_path().$folder; 
-    $name != null ? $filename = $name. '.' . $file_type : $filename = uniqid().'_'.time() . '.' . $file_type;
-    $url = public_path().$folder.$name;
+    $name != null ? $filename = $name : $filename = uniqid().'_'.time() ;
     
-    if ($file->move($destinationPath.'/' , $filename)) { 
+    $url = public_path().$folder.$filename;
+    
+    if ($file->move($destinationPath.'/',$filename. '.' . $file_type)) { 
 
+        //dd($url);
         $img = Image::make($url.".".$file_type);
         $w != null || $h != null ? $img->resize($w,$h) : "";
 
         if($file_type != 'png'){
             $img->save($url.".png",80,'png');
             File::delete($url.".".$file_type);
-        }else{ $img->save($url.".".$file_type);}
+        }else{ 
+            $img->save($url.".".$file_type);
+        }
         
         return $filename; 
     } 
